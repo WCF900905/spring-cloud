66f55b62c9d806ac5723e2db00f4672bef34f378
WCF <wuchengfeiss@126.com>
2018-02-27 14:57:07 +0800

    配置中心

--- eureka-feign/src/main/java/com/springcloud/eureka/Application.java
+++ eureka-feign/src/main/java/com/springcloud/eureka/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableFeignClients
 @RestController
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
