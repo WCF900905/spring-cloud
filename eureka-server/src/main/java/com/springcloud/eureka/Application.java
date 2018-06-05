7c90fbeddbac7caa18a69a2850cd0ad55b7604e4
WCF <wuchengfeiss@126.com>
2018-06-05 15:13:40 +0800

    1
    1

--- eureka-server/src/main/java/com/springcloud/eureka/Application.java
+++ eureka-server/src/main/java/com/springcloud/eureka/Application.java
@@ -2,10 +2,12 @@ package com.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
